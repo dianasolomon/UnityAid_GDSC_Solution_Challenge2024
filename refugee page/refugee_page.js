81c378be1defa18f3ad8449535e99178608406b0
Imrana <imranasumbul1211@gmail.com>
2024-02-25 18:05:39 +0530

    added certain things

--- refugee page/refugee_page.js
+++ refugee page/refugee_page.js
@@ -1,22 +1,20 @@
 let selectedCountry = document.getElementById("country");
-let netherlandHiddenList = document.querySelector(".netherlands-chosen")
-let selectedCountryValue  = selectedCountry.value;
+let netherlandHiddenList = document.querySelector(".list-of-topics");
+let germanyHiddenList = document.querySelector(".list-of-topics-germany");
 selectedCountry.addEventListener('change', function(){
-   
+   let selectedCountryValue  = selectedCountry.value;
    if(selectedCountryValue == 'germany'){
     
     window.location.href = 'https://www.expatica.com/de/working/employment-law/german-labour-law-and-work-contracts-104474/';
+    germanyHiddenList.style.display = "block";
+    netherlandHiddenList.style.display = "none";
    }
+
+   else if(selectedCountryValue == 'netherlands'){
+      germanyHiddenList.style.display = "none";
+      netherlandHiddenList.style.display = "block";
+   }
+   
 })
 console.log(selectedCountryValue);
 
-selectedCountry.addEventListener('change', function(){
-   if(selectedCountryValue == 'netherlands'){
-      netherlandHiddenList.style.display = "block";
-   }
-   else{
-      netherlandHiddenList.style.display = "none";
-   }
-   
-
-})
